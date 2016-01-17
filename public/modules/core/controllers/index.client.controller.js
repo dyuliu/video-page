19cdbde6dfbe93a06a58293617a25cb9ce959b5f
Yanhong Wu <wdan1990@gmail.com>
2016-01-17 23:36:12 +0800

    Finish basic system

--- public/modules/core/controllers/index.client.controller.js
+++ public/modules/core/controllers/index.client.controller.js
@@ -5,8 +5,8 @@ angular.module('core').controller('IndexController', ['$scope', '$http',
     function($scope, $http) {
         $scope.testDataList = [];
 
-        $http.get('./data/videoList.json').success(function(data) {
-            console.log(data);
+        $http.get('data/videoList.json').success(function(data) {
+            $scope.testDataList = data.slice(0, 12);
         });
     }
 ]);
--- public/modules/core/controllers/project.client.controller.js
+++ public/modules/core/controllers/project.client.controller.js
@@ -1,93 +1,22 @@
 'use strict';
 
 
-angular.module('core').controller('ProjectController', ['$scope',
-    function($scope) {
-        $scope.testDataList = [
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            },
-            {
-              title: 'Egoslider',
-              imageName: 'egoslider.png',
-              videoName: 'egoslider.mp4',
-              tag: ['graph'],
-              description: 'introduction to this video or your paper abstract'
-            }
-           ];
+angular.module('core').controller('ProjectController', ['$scope', '$http',
+    function($scope, $http) {
+        $scope.dataList = [];
+        $scope.currentPage = 1;
+        $scope.ITEMS_PER_PAGE = 9;
+        $scope.dataLen = 0;
+        $scope.curDataList = [];
+
+        $http.get('data/videoList.json').success(function(data) {
+            $scope.dataList = data;
+            $scope.dataLen = $scope.dataList.length;
+            $scope.curDataList = $scope.dataList.slice(($scope.currentPage - 1) * 9, Math.min($scope.dataLen, ($scope.currentPage - 1) * 9 + 9));
+        });
+
+        $scope.pageChanged = function() {
+            $scope.curDataList = $scope.dataList.slice(($scope.currentPage - 1) * 9, Math.min($scope.dataLen, ($scope.currentPage - 1) * 9 + 9));
+        };
     }
 ]);
